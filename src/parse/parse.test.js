1592b1ec7a9d987acf44ed996489cd599dd9e487
Jared Anderson <jared@tuxsudo.com>
2018-02-23 12:47:30 -0700

    Fix noon bug

--- src/parse/parse.test.js
+++ src/parse/parse.test.js
@@ -1,143 +1,148 @@
-import test from 'tape';
-import {parse} from './index.js';
+import test from "tape";
+import { parse } from "./index.js";
 
-test('parse', (t) => {
+test("parse", t => {
+  {
+    t.deepEquals(
+      Object.keys(parse("1")),
+      ["hours", "minutes", "seconds"],
+      "it returns an object with `hours`, `minutes`, and `seconds` props"
+    );
+  }
 
-    {
-        t.deepEquals(
-            Object.keys(parse('1')),
-            ['hours', 'minutes', 'seconds'],
-            'it returns an object with `hours`, `minutes`, and `seconds` props'
-        )
-    }
+  {
+    const { hours, minutes, seconds } = parse("17:11:31");
+    t.deepEquals(
+      { hours, minutes, seconds },
+      { hours: 17, minutes: 11, seconds: 31 },
+      "it parses 24hr format"
+    );
+  }
 
-    {
-        const {hours, minutes, seconds} = parse('17:11:31');
-        t.deepEquals(
-            {hours, minutes, seconds},
-            {hours: 17, minutes: 11, seconds: 31},
-            'it parses 24hr format'
-        );
-    }
+  {
+    const { hours, minutes, seconds } = parse("17:11");
+    t.deepEquals(
+      { hours, minutes, seconds },
+      { hours: 17, minutes: 11, seconds: 0 },
+      "when two segments, seconds defaults to 0"
+    );
+  }
 
-    {
-        const {hours, minutes, seconds} = parse('17:11');
-        t.deepEquals(
-            {hours, minutes, seconds},
-            {hours: 17, minutes: 11, seconds: 0},
-            'when two segments, seconds defaults to 0'
-        );
-    }
+  {
+    const { hours, minutes, seconds } = parse("23");
+    t.deepEquals(
+      { hours, minutes, seconds },
+      { hours: 23, minutes: 0, seconds: 0 },
+      "when 1 segment, minutes and seconds default to 0"
+    );
+  }
 
-    {
-        const {hours, minutes, seconds} = parse('23');
-        t.deepEquals(
-            {hours, minutes, seconds},
-            {hours: 23, minutes: 0, seconds: 0},
-            'when 1 segment, minutes and seconds default to 0'
-        );
-    }
+  {
+    const { hours, minutes } = parse("7:11a");
+    t.deepEquals(
+      { hours, minutes },
+      { hours: 7, minutes: 11 },
+      "it parses 12hr am"
+    );
+  }
 
+  {
+    const { hours, minutes } = parse("7:11p");
+    t.deepEquals(
+      { hours, minutes },
+      { hours: 19, minutes: 11 },
+      "it parses 12hr pm"
+    );
+  }
 
-    {
-        const {hours, minutes} = parse('7:11a');
-        t.deepEquals(
-            {hours, minutes},
-            {hours: 7, minutes: 11},
-            'it parses 12hr am'
-        );
+  {
+    t.deepEquals(
+      parse("1:00p"),
+      parse("1:00pm"),
+      "meridiem can be 1 or two chars"
+    );
+  }
 
-    }
+  {
+    t.deepEquals(
+      parse("1:00A"),
+      parse("1:00am"),
+      "meridiem can upper or lower case"
+    );
+  }
 
-    {
-        const {hours, minutes} = parse('7:11p');
-        t.deepEquals(
-            {hours, minutes},
-            {hours: 19, minutes: 11},
-            'it parses 12hr pm'
-        );
+  {
+    const { hours, minutes } = parse("1p");
+    t.deepEquals(
+      { hours, minutes },
+      { hours: 13, minutes: 0 },
+      "minutes is optionals"
+    );
+  }
 
-    }
+  {
+    const { hours, minutes, seconds } = parse("13045pm");
+    t.deepEquals(
+      { hours, minutes, seconds },
+      { hours: 13, minutes: 30, seconds: 45 },
+      "works without colons and spaces (12hr, 3segments)"
+    );
+  }
 
-    {
-        t.deepEquals(
-            parse('1:00p'),
-            parse('1:00pm'),
-            'meridiem can be 1 or two chars'
-        );
+  {
+    const { hours, minutes, seconds } = parse("130pm");
+    t.deepEquals(
+      { hours, minutes, seconds },
+      { hours: 13, minutes: 30, seconds: 0 },
+      "works without colons and spaces (12hr, 2segments)"
+    );
+  }
 
-    }
+  {
+    const { hours, minutes, seconds } = parse("222134");
+    t.deepEquals(
+      { hours, minutes, seconds },
+      { hours: 22, minutes: 21, seconds: 34 },
+      "works without colons and spaces (24hr, 3segments)"
+    );
+  }
 
-    {
-        t.deepEquals(
-            parse('1:00A'),
-            parse('1:00am'),
-            'meridiem can upper or lower case'
-        );
+  {
+    const { hours, minutes, seconds } = parse("2312");
+    t.deepEquals(
+      { hours, minutes, seconds },
+      { hours: 23, minutes: 12, seconds: 0 },
+      "works without colons and spaces (24hr, 2segments)"
+    );
+  }
 
-    }
+  {
+    const { hours, minutes, seconds } = parse("12:00");
+    t.deepEquals(
+      { hours, minutes, seconds },
+      { hours: 12, minutes: 0, seconds: 0 },
+      "handles noonish correctly"
+    );
+  }
 
-    {
-        const {hours, minutes} = parse('1p');
-        t.deepEquals(
-            {hours, minutes},
-            {hours: 13, minutes: 0},
-            'minutes is optionals'
-        );
-    }
+  {
+    const { hours, minutes, seconds } = parse("00:00");
+    t.deepEquals(
+      { hours, minutes, seconds },
+      { hours: 0, minutes: 0, seconds: 0 },
+      "handles midnight correctly"
+    );
+  }
 
-    {
-        const {hours, minutes, seconds} = parse('13045pm');
-        t.deepEquals(
-            {hours, minutes, seconds},
-            {hours: 13, minutes: 30, seconds: 45},
-            'works without colons and spaces (12hr, 3segments)'
-        );
-    }
+  {
+    const { hours } = parse(9);
+    t.equal(hours, 9, "coerces numbers to strings");
+  }
 
-    {
-        const {hours, minutes, seconds} = parse('130pm');
-        t.deepEquals(
-            {hours, minutes, seconds},
-            {hours: 13, minutes: 30, seconds: 0},
-            'works without colons and spaces (12hr, 2segments)'
-        );
-    }
+  {
+    const results = parse("abc");
+    t.equal(results, null, "returns null when cant parse");
+  }
 
-    {
-        const {hours, minutes, seconds} = parse('222134');
-        t.deepEquals(
-            {hours, minutes, seconds},
-            {hours: 22, minutes: 21, seconds: 34},
-            'works without colons and spaces (24hr, 3segments)'
-        );
-    }
-
-    {
-        const {hours, minutes, seconds} = parse('2312');
-        t.deepEquals(
-            {hours, minutes, seconds},
-            {hours: 23, minutes: 12, seconds: 0},
-            'works without colons and spaces (24hr, 2segments)'
-        );
-    }
-
-    {
-        const {hours} = parse(9);
-        t.equal(hours, 9, 'coerces numbers to strings');
-    }
-
-
-    {
-        const results = parse('abc');
-        t.equal(results, null, 'returns null when cant parse');
-    }
-
-
-
-
-
-
-
-    t.end();
-})
+  t.end();
+});
--- src/parse/parseString.js
+++ src/parse/parseString.js
@@ -1,35 +1,28 @@
-export const parseString = (input="") => {
+export const parseString = (input = "") => {
+  const [
+    ,
+    hoursString,
+    minutesString = "0",
+    secondsString = "0",
+    meridiemString
+  ] =
+    input
+      .toString()
+      .match(/^(\d{1,2})\s*[:]?\s*(\d{2})?\s*[:]?\s*(\d{2})?\s*([ap]m?)?$/i) ||
+    [];
 
-    const [
-        ,
-        hoursString,
-        minutesString="0",
-        secondsString="0",
-        meridiemString="am"
-    ] = (input)
-        .toString()
-        .match(/^(\d{1,2})\s*[:]?\s*(\d{2})?\s*[:]?\s*(\d{2})?\s*([ap]m?)?$/i)
-        || []
-    ;
+  if (hoursString && minutesString) {
+    const parsedHours = parseInt(hoursString);
+    const isPM = meridiemString && /^pm?$/i.test(meridiemString);
+    const diff = isPM && parsedHours !== 12 ? 12 : 0;
+    const seconds = parseInt(secondsString) % 60;
+    const minutes = parseInt(minutesString) % 60;
+    const hours = parsedHours + diff;
 
-    if(hoursString && minutesString) {
-        const parsedHours = parseInt(hoursString);
-        const isPM = /^pm?$/i.test(meridiemString);
-        const diff = isPM ? 12 : 0;
-        const seconds = parseInt(secondsString) % 60;
-        const minutes = parseInt(minutesString) % 60;
-        const hours = isPM && parsedHours === 12 // if they said 12pm
-            ? 12 // leave it at 12
-            : !isPM && parsedHours === 12 || parsedHours === 0 // they explicity said 12am or 0
-                ? 0 // make it 0
-                // otherwise...
-                : parsedHours + diff; // add 12 if PM, 0 if not
+    return { hours, minutes, seconds };
+  }
 
-        return { hours, minutes, seconds }
-
-    }
-
-    return null;
-}
+  return null;
+};
 
 export default parseString;
